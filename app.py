6ff7e5d9916572a9aee4d6ecb2f628e8c265980e
Matthew Berthoud <mwberthoud@wm.edu>
2024-08-17 10:59:29 -0400

    connect and disconnect socket stuff

--- app.py
+++ app.py
@@ -56,10 +56,39 @@ def home():
 @app.route("/room")
 def room():
     room = session.get("room")
-    print("room", room)
     if room is None or session.get("name") is None or room not in rooms:
         return redirect(url_for("home"))
     return render_template("room.html")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+    
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} left room {room}")
+
 
 # @app.route('/', methods=['GET', 'POST'])
 # def index():
